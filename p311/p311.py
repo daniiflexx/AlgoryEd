91d9608d3ed6fb1ebe890e6daf47c8be819790c1
daniiflexx <danicruzflex@gmail.com>
2022-12-18 22:59:11 +0100

    comentado

--- p311/p311.py
+++ p311/p311.py
@@ -5,12 +5,25 @@ import itertools
 from typing import List, Tuple, Dict, Callable, Iterable, Union
 
 def split(t: List) -> tuple[List, int, List]:
+    """ Función que devuelve una tupla con las listas de elementos menores y mayores que el pivote, además del pivote
+    Args:
+        t (List): lista con la tabla original
+    Returns: 
+        tuple[List, int, List]: lista de menores, el pivote, lista de mayores
+    """
     mid = t[0]
     t_l = [u for u in t if u < mid]
     t_r = [u for u in t if u > mid]
     return (t_l, mid, t_r)
 
 def qsel(t: np.ndarray, k: int)-> Union[int, None]:
+    """ Función que devuelve el elemento que corresponde al índice de la tabla ordenada
+    Args:
+        t (np.ndarray): la tabla ordenada
+        k: el índice del elemento a devolver
+    Returns: 
+        Union(int, None): elemento a devolver
+    """
     if k >= len(t) or k < 0:
         return
     if len(t) == 1 and k == 0:
@@ -26,6 +39,14 @@ def qsel(t: np.ndarray, k: int)-> Union[int, None]:
         return qsel(t_r, k-m-1)
 
 def qsel_nr(t: np.ndarray, k: int)-> Union[int, None]:
+    """ Función que devuelve el elemento que corresponde al índice de la tabla ordenada sin recursividad
+    Args:
+        t (np.ndarray): la tabla ordenada
+        k: el índice del elemento a devolver
+    Returns: 
+        Union(int, None): elemento a devolver
+    """
+
     tt = t.copy()
     while len(tt) > 0:
         if len(tt) == 1 and k == 0:
@@ -42,11 +63,25 @@ def qsel_nr(t: np.ndarray, k: int)-> Union[int, None]:
     return
 
 def split_pivot(t: np.ndarray, mid: int)-> Tuple[np.ndarray, int, np.ndarray]:
+    """ Función que devuelve una tupla con las listas de elementos menores y mayores que el pivote, además del pivote
+    Args:
+        t (np.ndarray): lista con la tabla original
+    Returns: 
+        tuple[np.ndarray, int, np.ndarray]: lista de menores, el pivote, lista de mayores
+    """
+
     s1 = [i for i in t if i < mid]
     s2 = [i for i in t if i > mid]
     return s1, mid, s2
 
 def pivot5(t: np.ndarray)-> int:
+    """ Función que escoge el mejor pivote(mediana de medianas)
+    Args:
+        t (np.ndarray): lista con la tabla original
+    Returns: 
+        int: pivote
+    """
+
     if len(t) > 5:
         group_size = 5
         n_group = len(t) // group_size
@@ -66,6 +101,14 @@ def pivot5(t: np.ndarray)-> int:
         return np.sort(t)[len(t)//2]
 
 def qsel5_nr(t: np.ndarray, k: int)-> Union[int, None]:
+    """ Función que devuelve el elemento que corresponde al índice de la tabla ordenada sin recursividad, usando pivot5
+    Args:
+        t (np.ndarray): la tabla ordenada
+        k: el índice del elemento a devolver
+    Returns: 
+        Union(int, None): elemento a devolver
+    """
+
     if k >= len(t) or k < 0:
         return
     
@@ -88,6 +131,12 @@ def qsel5_nr(t: np.ndarray, k: int)-> Union[int, None]:
         return int(np.sort(tt)[k])
 
 def qsort_5(t: np.ndarray)-> np.ndarray:
+    """ Función quicksort para ordenar una tabla, usando pivot5
+    Args:
+        t (np.ndarray): la tabla a ordenar
+    Returns: 
+        np.ndarray: la tabla ordenada
+    """
     
     if t is None:
         return None
